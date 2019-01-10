1e64388343a9d5c1cdc334170982d3ad6d5f65fa
Jalen-G <46547996+Jalen-G@users.noreply.github.com>
2019-01-10 09:11:00 -0600

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,4 +1,4 @@
-import requests, smtplib, datetime, time
+import requests, smtplib, datetime, time, pytemperature
 
 # Send text message through SMS gateway of destination number
 while True:
@@ -8,27 +8,26 @@ while True:
     time.sleep(1)
     print(time_now)
 
-    if time_now == (8, 30, 0):
-        server = smtplib.SMTP( "smtp.gmail.com", 587 )
-
+    if time_now == (9, 9, 25):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
-
         server.login('***@gmail.com', '***')
-        
-        api_address = "http://api.openweathermap.org/data/2.5/weather?lat=***&lon=-***&appid=***&units=imperial"
 
+        api_address = "http://api.openweathermap.org/data/2.5/weather?lat=***&lon=***&appid=***"
         json_data = requests.get(api_address).json()
 
         weather = json_data["weather"][0]["main"]
-        temp = str(round(json_data["main"]["temp"]))
+        temp = json_data["main"]["temp"]
         wind = str(round(json_data['wind']['speed']))
-        max_temp = str(round(json_data["main"]["temp_max"]))
-        min_temp = str(round(json_data["main"]["temp_min"]))
+        max_temp = json_data["main"]["temp_max"]
+        min_temp = json_data["main"]["temp_min"]
 
-        server.sendmail( '***@gmail.com', '***@mms.att.net', "It is " + temp + " degrees and " + weather + " outside right now with a max of " + max_temp + " and a low of " + min_temp + ". The wind is blowing at " + wind + " MPH.")
-       
+        temp_f = str(round(pytemperature.k2f(temp)))
+        max_f = str(round(pytemperature.k2f(max_temp)))
+        min_f = str(round(pytemperature.k2f(min_temp)))
+
+        server.sendmail( '***@gmail.com', '***@mms.att.net', "It is " + temp_f + " degrees and " + weather + " outside right now with a max of " + max_f + " and a low of " + min_f + ". The wind is blowing at " + wind + " MPH.")
         time.sleep(5)
-        
         print("sent!")
 
 
