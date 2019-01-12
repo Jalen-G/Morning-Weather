de259249f0df344d00b22db8fa6aac7f6e0e15ea
Jalen-G <46547996+Jalen-G@users.noreply.github.com>
2019-01-12 17:21:20 -0600

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,5 @@
-import requests, smtplib, datetime, time, pytemperature
+import requests, smtplib, datetime, time
 
-# Send text message through SMS gateway of destination number
 while True:
 
     now = datetime.datetime.now()
@@ -8,25 +7,24 @@ while True:
     time.sleep(1)
     print(time_now)
 
-    if time_now == (9, 9, 25):
+    if time_now == (8, 0, 0):
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.starttls()
         server.login('***@gmail.com', '***')
 
-        api_address = "http://api.openweathermap.org/data/2.5/weather?lat=***&lon=***&appid=***"
+        api_address = "***"
+        current_api = "***"
+
         json_data = requests.get(api_address).json()
+        current_data = requests.get(current_api).json()
 
-        weather = json_data["weather"][0]["main"]
-        temp = json_data["main"]["temp"]
-        wind = str(round(json_data['wind']['speed']))
-        max_temp = json_data["main"]["temp_max"]
-        min_temp = json_data["main"]["temp_min"]
+        weather = json_data['Headline']['Text']
+        max_temp = str(int(json_data['DailyForecasts'][0]['Temperature']['Maximum']['Value']))
+        min_temp = str(int(json_data['DailyForecasts'][0]['Temperature']['Minimum']['Value']))
+        temp = str(int(current_data[0]['Temperature']['Imperial']['Value']))
 
-        temp_f = str(round(pytemperature.k2f(temp)))
-        max_f = str(round(pytemperature.k2f(max_temp)))
-        min_f = str(round(pytemperature.k2f(min_temp)))
 
-        server.sendmail( '***@gmail.com', '***@mms.att.net', "It is " + temp_f + " degrees and " + weather + " outside right now with a max of " + max_f + " and a low of " + min_f + ". The wind is blowing at " + wind + " MPH.")
+        server.sendmail( '***@gmail.com', '***@mms.att.net', weather + ". Currently it's " + temp + " degrees outside with a high of " + max_temp + " and a low of " + min_temp +".")
         time.sleep(5)
         print("sent!")
 
